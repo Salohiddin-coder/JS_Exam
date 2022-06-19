c8d4113cadee7171a46341cdaf0d75603ea2e8b9
Salohiddin-coder <neocrazy.gamer@gmail.com>
2022-06-19 15:14:01 +0500

    add new changes

--- taskfour.js
+++ taskfour.js
@@ -1,11 +1,13 @@
 function maxPrice(array) {
-  let max = "";
+  let maxSum = 0;
+  let maxSumName = '';
   for (let i = 0; i < array.length; i++) {
-    if (array[i].price > max) {
-      max = array[i].name;
+    if (array[i].price > maxSum) {
+      maxSum = array[i].price;
+      maxSumName = array[i].name;
     }
   }
-  return max;
+  return maxSumName;
 }
 
-console.log(maxPrice([{ name: 'apples', price: 3 }, { name: 'bananas', price: 4 }, { name: 'oranges', price: 5 }]));
+console.log(maxPrice([{ name: 'apples', price: 3 }, { name: 'bananas', price: 7 }, { name: 'oranges', price: 5 }]));
--- taskthree.js
+++ taskthree.js
@@ -1,8 +1,15 @@
 function positiveSum(arr) {
   let sum = 0;
+  let negativeCount = 0;
   for (let i = 0; i < arr.length; i++) {
       if (arr[i] > 0) {
           sum += arr[i];
+      }
+      if (arr[i] < 0) {
+          negativeCount++;
+      }
+      if (negativeCount === arr.length) {
+          return 0;
       }
   }
   return sum;
